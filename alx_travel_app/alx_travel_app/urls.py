ceb14fe43c931fa6c1753ab2e10ff6190fc5e940
p-prime1 <preciousnwaka95@gmail.com>
2025-07-02 10:47:34 +0100

    first commit

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -14,24 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from rest_framework import permissions
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.info(
-        title='Listings API',
-        default_version='v1',
-        description='API documentation for Listings project'
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger/', schema_view.width_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("admin/", admin.site.urls),
 ]
